d9b0f19e86ce6d146d8e924b52aa6473d7b081a8
Marian Catalin Rencsik <mrencsik@greentube.com>
2026-05-19 09:01:57 +0200

    Update to capacitor 8

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorUsercentrics",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapacitorUsercentrics",
             targets: ["CapacitorUsercentricsPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
         .package(url: "https://bitbucket.org/usercentricscode/usercentrics-spm-ui.git", from: "2.24.4")
     ],
     targets: [
